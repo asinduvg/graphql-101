13ac00c48d34d5c23dd633790b704e26185c1424
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2024-05-12 00:21:44 +0530

    Queries

--- job-board/client/src/pages/CompanyPage.js
+++ job-board/client/src/pages/CompanyPage.js
@@ -1,18 +1,25 @@
-import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { getCompany } from "../lib/graphql/queries";
+import JobList from "../components/JobList";
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [company, setCompany] = useState();
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  }, [companyId]);
 
-  const company = companies.find((company) => company.id === companyId);
+  if (!company) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
-      <h1 className="title">
-        {company.name}
-      </h1>
-      <div className="box">
-        {company.description}
-      </div>
+      <h1 className="title">{company.name}</h1>
+      <div className="box">{company.description}</div>
+      <h2 className="title is-5">Jobs at {company.name}</h2>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -1,8 +1,10 @@
 import { getCompany } from "./db/companies.js";
-import { getJobs } from "./db/jobs.js";
+import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
 
 export const resolvers = {
   Query: {
+    company: (_root, { id }) => getCompany(id),
+    job: (_root, { id }) => getJob(id),
     jobs: () => getJobs(),
   },
 
@@ -10,6 +12,11 @@ export const resolvers = {
     company: (job) => getCompany(job.companyId),
     date: (job) => toISODate(job.createdAt),
   },
+
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+
 };
 
 function toISODate(value) {
